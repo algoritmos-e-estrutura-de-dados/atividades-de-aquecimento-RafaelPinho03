ea39e08d7506102c23768bffc166f9a9e0c6340b
Rafael Pinho <89493675+RafaelPinho03@users.noreply.github.com>
2022-08-28 22:51:57 -0300

    EXERCÍCIOS.NOVOS

---
+++ exercicios/media2.py
@@ -0,0 +1,11 @@
+a = float(input("Nota 1: "))
+b = float(input("Nota 2: "))
+c = float(input("Nota 3: "))
+
+notaA = (a * 2)
+notaB = (b * 3)
+notaC = (c * 5)
+notaFinal = ( notaA + notaB + notaC) /10    
+
+
+print(f"MEDIA = {notaFinal:,.1f}")
