07deb938688a359305cf83af81a771fc31dd0b81
Rafael Pinho <89493675+RafaelPinho03@users.noreply.github.com>
2022-08-28 22:57:21 -0300

    EXERCÍCIOS.NOVOS

---
+++ exercicios/salario.py
@@ -0,0 +1,5 @@
+A = int(input("Número do funcionário: "))
+B = int(input("Número de horas trabalhada: "))
+C = float(input("Valor recebido por hora: "))
+
+print(f"O funcionário {A}, deve receber {B * C:,.2f}")
